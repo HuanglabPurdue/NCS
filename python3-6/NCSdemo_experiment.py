69a29cd518048c71c68cafb554c60aa97ffe1c03
mill1561 <mill1561@purdue.edu>
2017-08-28 18:43:17 -0400

    update Python demo code

--- python3-6/NCSdemo_experiment.py
+++ python3-6/NCSdemo_experiment.py
@@ -20,16 +20,16 @@ if __name__ == "__main__":
     gain = fmat_gain['gain']
     ccdoffset = fmat_gain['ccdoffset']
     data = fmat_data['ims']
-    R = 128
+    imgsz = 128
     rectx = 59
     recty = 58
     
-    subims = ncs.cropimage(data,R,rectx,recty)
-    subvar = ncs.cropimage(ccdvar,R,rectx,recty)
-    suboffset = ncs.cropimage(ccdoffset,R,rectx,recty)
-    subgain = ncs.cropimage(gain,R,rectx,recty)
+    subims = ncs.cropimage(data,imgsz,rectx,recty)
+    subvar = ncs.cropimage(ccdvar,imgsz,rectx,recty)
+    suboffset = ncs.cropimage(ccdoffset,imgsz,rectx,recty)
+    subgain = ncs.cropimage(gain,imgsz,rectx,recty)
     subgain[subgain<1] = 1
-    subims = subims.swapaxes(0,2) # the subims has a shape of [20,R,R], frame dimension is the first dimension
+    subims = subims.swapaxes(0,2) # the subims has a shape of [20,imgsz,imgsz], frame dimension is the first dimension
     subims = subims.swapaxes(1,2)
     N = subims.shape[0]
     imsd = (subims-np.tile(suboffset,(N,1,1)))/np.tile(subgain,(N,1,1))
@@ -37,13 +37,13 @@ if __name__ == "__main__":
     
     
     Rs = 8
-    pixelsize = 0.091
+    Pixelsize = 0.091
     Lambda = 0.54
     NA = 1.35
     iterationN = 15
     alpha = 0.2
     
-    out = ncs.reducenoise(Rs,imsd[0:1],subvar,subgain,R,pixelsize,NA,Lambda,alpha,iterationN)
+    out = ncs.reducenoise(Rs,imsd[0:1],subvar,subgain,imgsz,Pixelsize,NA,Lambda,alpha,iterationN)
 
     f,(ax1,ax2) = plt.subplots(1,2,sharey=False)
     ax1.imshow(imsd[0],aspect='equal',cmap=plt.cm.gray)
--- python3-6/NCSdemo_simulation.py
+++ python3-6/NCSdemo_simulation.py
@@ -16,13 +16,13 @@ if __name__ == "__main__":
 # load coordinates of microtubules 
 
     fpath1 = r'randwlcposition.mat'
-    R = 128
+    imgsz = 128
     zoom = 8
-    pixelsize = 0.1
+    Pixelsize = 0.1
     NA = 1.4
     Lambda = 0.7
     t = time.time()
-    res = ncs.genidealimage(R,pixelsize,zoom,NA,Lambda,fpath1)
+    res = ncs.genidealimage(imgsz,Pixelsize,zoom,NA,Lambda,fpath1)
     elapsed = time.time()-t
     print('Elapsed time for generating ideal image:', elapsed)
     imso = res[0]
@@ -34,20 +34,19 @@ if __name__ == "__main__":
     bg = 10
     offset = 100
     N = 1
-    noisemap = ncs.gennoisemap(R,fpath)
+    noisemap = ncs.gennoisemap(imgsz,fpath)
     varsub = noisemap[0]*10
     gainsub = noisemap[1]
     dataimg = ncs.gendatastack(imso,varsub,gainsub,I,bg,offset,N)
     imsd = dataimg[1]
 
-    pixelsize = 0.1
     NA = 1.4
     Lambda = 0.7
     Rs = 8
     iterationN = 15
     alpha = 0.1
     
-    out = ncs.reducenoise(Rs,imsd[0:1],varsub,gainsub,R,pixelsize,NA,Lambda,alpha,iterationN)
+    out = ncs.reducenoise(Rs,imsd[0:1],varsub,gainsub,imgsz,Pixelsize,NA,Lambda,alpha,iterationN)
 
     f,(ax1,ax2) = plt.subplots(1,2,sharey=False)
     ax1.imshow(imsd[0],aspect='equal',cmap=plt.cm.gray)
