9f9c47daf3d1b111c8f1543bcba38b28a80e0a3a
Hazen Babcock <hbabcock@fas.harvard.edu>
2019-04-21 19:55:06 -0400

    Remove storm-analysis import. Fix comments.

--- clib/pyCNCS/loadclib.py
+++ clib/pyCNCS/loadclib.py
@@ -10,13 +10,12 @@ import sys
 import os
 import re
 
-import storm_analysis
 
 def loadNCSCLibrary():
     library_filename = "ncs"
 
     #
-    # This assumes that the C library is one level up from where this file is.
+    # This assumes that the C libraries are in this directory.
     #
     c_lib_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -27,8 +26,8 @@ def loadNCSCLibrary():
 
         # Try to load the library without fiddling with the Windows DLL search 
         # first. Do this so that if the user wanted to use their own versions 
-        # of FFTW or LAPACK these will get loaded instead of the ones in 
-        # storm-analysis.
+        # of FFTW or LBFGS these will get loaded instead of the ones in 
+        # clib/pyCNCS.
         try:
             # This suppresses the Windows DLL missing dialog.
             ctypes.windll.kernel32.SetErrorMode(0x0001|0x0002|0x8000)
@@ -37,7 +36,7 @@ def loadNCSCLibrary():
             # Unsuppress the Windows DLL missing dialog.
             ctypes.windll.kernel32.SetErrorMode(0)
 
-            # Push the storm-analysis directory into the DLL search path.
+            # Push the clib/pyCNCS directory into the DLL search path.
             if (sys.version_info > (3, 0)):
                 ctypes.windll.kernel32.SetDllDirectoryW(c_lib_path)
             else:
